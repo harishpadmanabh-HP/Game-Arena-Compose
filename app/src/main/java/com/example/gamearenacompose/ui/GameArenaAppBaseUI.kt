a2c43780a8efa316ec116aeafb72223135b223e0
harishPadmanabh <harish.p@accubits.com>
2021-11-03 21:47:47 +0530

    add navigations in all game screen

--- app/src/main/java/com/example/gamearenacompose/ui/GameArenaAppBaseUI.kt
+++ app/src/main/java/com/example/gamearenacompose/ui/GameArenaAppBaseUI.kt
@@ -1,16 +1,19 @@
 package com.example.gamearenacompose.ui
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.gamearenacompose.ui.theme.GameArenaComposeTheme
+import com.example.gamearenacompose.ui.theme.darkBLue
 import com.example.gamearenacompose.utils.WindowSize
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 
+@ExperimentalFoundationApi
 @ExperimentalPagerApi
 @Composable
 fun GameArenaAppUi(
@@ -20,7 +23,7 @@ fun GameArenaAppUi(
         val systemUiController = rememberSystemUiController()
         val darkIcons = MaterialTheme.colors.isLight
         SideEffect {
-            systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = darkIcons)
+            systemUiController.setSystemBarsColor(darkBLue, darkIcons = darkIcons)
         }
         val navController = rememberNavController()
         val navigationActions = remember(navController) {
